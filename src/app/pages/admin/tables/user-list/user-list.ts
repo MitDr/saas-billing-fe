2762f3d078339b948a7032b7185daeb49653f398
Long Tran <ttlong1301@gmail.com>
2026-02-02 15:04:35 +0700

    02/02/2025 finish form example for tables/users

--- src/app/pages/admin/tables/user-list/user-list.ts
+++ src/app/pages/admin/tables/user-list/user-list.ts
@@ -1,24 +1,30 @@
-import {Component, input} from '@angular/core';
+import {Component, input, OnInit, signal} from '@angular/core';
 import {User} from '../../../../core/interface/user';
-import {NzCardComponent} from 'ng-zorro-antd/card';
 import {NzTableModule} from 'ng-zorro-antd/table';
 import {NzTagComponent} from 'ng-zorro-antd/tag';
 import {NzPopconfirmDirective} from 'ng-zorro-antd/popconfirm';
 import {NzButtonComponent} from 'ng-zorro-antd/button';
+import {NzInputDirective} from 'ng-zorro-antd/input';
+import {FormsModule} from '@angular/forms';
+import {NzOptionComponent, NzSelectComponent} from 'ng-zorro-antd/select';
 
 @Component({
   selector: 'app-user-list',
   imports: [
-    NzCardComponent,
     NzTableModule,
     NzTagComponent,
     NzPopconfirmDirective,
     NzButtonComponent,
+    NzInputDirective,
+    FormsModule,
+    NzOptionComponent,
+    NzSelectComponent,
   ],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css',
 })
-export class UserList {
+export class UserList implements OnInit{
+
   users:User[]=FAKE_USERS;
 
   checked= false;
@@ -26,6 +32,15 @@ export class UserList {
   indeterminate = false;
   listOfCurrentData:User[] = [];
   setOfCheckedId= new Set<number>;
+  editing = signal(false);
+  ROLE = ROLE_OPTIONS
+
+  editCache: {
+    [key: string]:{
+      edit: boolean;
+      data: User
+    }
+  } = {}
 
   updateCheckedSet(id:number, checked:boolean){
     if (checked){
@@ -57,11 +72,50 @@ export class UserList {
       .forEach(({ id }) => this.updateCheckedSet(id, checked));
     this.refreshCheckedStatus();
   }
+
+  startEdit(id: number){
+    this.editCache[id].edit = true;
+    this.editing.update(()=>true);
+  }
+
+  cancelEdit(id: number){
+    const index = this.users.findIndex(item => item.id === id);
+    this.editCache[id] = {
+      data: {...this.users[index]
+      },
+      edit: false
+    };
+    this.editing.update(()=>false);
+  }
+
+  saveEdit(id: number){
+    const index = this.users.findIndex(item => item.id === id);
+    Object.assign(this.users[index], this.editCache[id].data);
+    this.editCache[id].edit = false;
+    this.editing.update(()=>false)
+  }
+
+  updateEditCache(){
+    this.users.forEach(item => {
+      this.editCache[item.id] = {
+        edit: false,
+        data: { ...item }
+      };
+    });
+  }
+
+  ngOnInit(): void {
+    this.updateEditCache();
+  }
+
+  multipleDelete(){
+    console.log("Delete");
+  }
 }
 
 export const FAKE_USERS: User[] = [
   {
-    id: 2,
+    id: 1,
     username: 'alice',
     email: 'alice@gmail.com',
     role: 'USER',
@@ -69,7 +123,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '06-10-2025 11:22:40'
   },
   {
-    id: 3,
+    id: 2,
     username: 'bob',
     email: 'bob@gmail.com',
     role: 'USER',
@@ -77,7 +131,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '01-10-2025 08:45:00'
   },
   {
-    id: 2,
+    id: 3,
     username: 'alice',
     email: 'alice@gmail.com',
     role: 'USER',
@@ -85,7 +139,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '06-10-2025 11:22:40'
   },
   {
-    id: 3,
+    id: 4,
     username: 'bob',
     email: 'bob@gmail.com',
     role: 'USER',
@@ -93,7 +147,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '01-10-2025 08:45:00'
   },
   {
-    id: 2,
+    id: 5,
     username: 'alice',
     email: 'alice@gmail.com',
     role: 'USER',
@@ -101,7 +155,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '06-10-2025 11:22:40'
   },
   {
-    id: 3,
+    id: 6,
     username: 'bob',
     email: 'bob@gmail.com',
     role: 'USER',
@@ -109,7 +163,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '01-10-2025 08:45:00'
   },
   {
-    id: 2,
+    id: 7,
     username: 'alice',
     email: 'alice@gmail.com',
     role: 'USER',
@@ -117,7 +171,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '06-10-2025 11:22:40'
   },
   {
-    id: 3,
+    id: 8,
     username: 'bob',
     email: 'bob@gmail.com',
     role: 'USER',
@@ -125,7 +179,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '01-10-2025 08:45:00'
   },
   {
-    id: 2,
+    id: 9,
     username: 'alice',
     email: 'alice@gmail.com',
     role: 'USER',
@@ -133,7 +187,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '06-10-2025 11:22:40'
   },
   {
-    id: 3,
+    id: 10,
     username: 'bob',
     email: 'bob@gmail.com',
     role: 'USER',
@@ -141,7 +195,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '01-10-2025 08:45:00'
   },
   {
-    id: 2,
+    id: 11,
     username: 'alice',
     email: 'alice@gmail.com',
     role: 'USER',
@@ -149,7 +203,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '06-10-2025 11:22:40'
   },
   {
-    id: 3,
+    id: 12,
     username: 'bob',
     email: 'bob@gmail.com',
     role: 'USER',
@@ -157,7 +211,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '01-10-2025 08:45:00'
   },
   {
-    id: 2,
+    id: 13,
     username: 'alice',
     email: 'alice@gmail.com',
     role: 'USER',
@@ -165,7 +219,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '06-10-2025 11:22:40'
   },
   {
-    id: 3,
+    id: 14,
     username: 'bob',
     email: 'bob@gmail.com',
     role: 'USER',
@@ -173,7 +227,7 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '01-10-2025 08:45:00'
   },
   {
-    id: 2,
+    id: 15,
     username: 'alice',
     email: 'alice@gmail.com',
     role: 'USER',
@@ -181,12 +235,19 @@ export const FAKE_USERS: User[] = [
     modifiedDate: '06-10-2025 11:22:40'
   },
   {
-    id: 3,
+    id: 16,
     username: 'bob',
     email: 'bob@gmail.com',
     role: 'USER',
     createdDate: '01-10-2025 08:45:00',
     modifiedDate: '01-10-2025 08:45:00'
   }
+];
+
+export type Role = 'ADMIN' | 'USER';
+
+export const ROLE_OPTIONS = [
+  { label: 'Admin', value: 'ADMIN', color: 'blue' },
+  { label: 'User', value: 'USER', color: 'green' }
 ];
 
